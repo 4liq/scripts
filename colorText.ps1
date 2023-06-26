ab422bb5bc18be8f8141230cfd4b442c59c3ee48
Albin <66908606+abebalmao@users.noreply.github.com>
2023-06-26 13:35:44 +0200

    Create colorText.ps1

---
+++ colorText.ps1
@@ -0,0 +1,36 @@
+function to_red ($msg) {
+    return "$([char]0x1b)[91m$msg$([char]0x1b)[0m"
+}
+
+function to_green ($msg) {
+    return "$([char]0x1b)[92m$msg$([char]0x1b)[0m"
+}
+
+function to_yellow ($msg) {
+    return "$([char]0x1b)[93m$msg$([char]0x1b)[0m"
+}
+
+function to_blue ($msg) {
+    return "$([char]0x1b)[94m$msg$([char]0x1b)[0m"
+}
+
+function to_purple ($msg) {
+    return "$([char]0x1b)[95m$msg$([char]0x1b)[0m"
+}
+   
+function to_cyan ($msg) {
+    return "$([char]0x1b)[96m$msg$([char]0x1b)[0m"
+}
+
+function to_white ($msg) {
+    return "$([char]0x1b)[97m$msg$([char]0x1b)[0m"
+}
+
+function to_lightgrey ($msg) {
+    return "$([char]0x1b)[98m$msg$([char]0x1b)[0m"
+}
+
+
+
+
+
